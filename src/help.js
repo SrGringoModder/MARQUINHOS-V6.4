a86ae31e2739f3130072ffd0d91f851f7623d5ef
Lynx Mods <76623560+SrGringoModder@users.noreply.github.com>
2021-01-16 15:36:41 -0400

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,233 @@
+consthelp=(prefix)=>{
+	return`
+╔═════════════════
+║「Marquinhos Bot」
+╠═════════════════
+║╭────⊱◈ INFO ◈⊰─────»
+║├─➣ Prefix:「${prefix}」
+║├─➣ Criador : Sr Gringo Mods
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ SOBRE ◈⊰─────»
+║│
+║├─➣  ${prefix}info
+║├─➣  ${prefix}blocklist
+║├─➣  ${prefix}chatlist
+║├─➣  ${prefix}ping
+║├─➣  ${prefix}bugreport
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ CRIAR ◈⊰────»
+║│
+║├─➣  ${prefix}sticker
+║├─➣  ${prefix}stickergif
+║├─➣  ${prefix}toimg
+║├─➣  ${prefix}tomp3
+║├─➣  ${prefix}bpink
+║├─➣  ${prefix}marvellogo
+║├─➣  ${prefix}snowwrite
+║├─➣  ${prefix}3dtext
+║├─➣  ${prefix}ninjalogo
+║├─➣  ${prefix}water
+║├─➣  ${prefix}firetext
+║├─➣  ${prefix}logowolf
+║├─➣  ${prefix}logowolf2
+║├─➣  ${prefix}phlogo
+║├─➣  ${prefix}glitch
+║├─➣  ${prefix}neonlogo
+║├─➣  ${prefix}neonlogo2
+║├─➣  ${prefix}lionlogo
+║├─➣  ${prefix}jokerlogo
+║├─➣  ${prefix}shadow
+║├─➣  ${prefix}burnpaper
+║├─➣  ${prefix}coffee
+║├─➣  ${prefix}lovepaper
+║├─➣  ${prefix}woodblock
+║├─➣  ${prefix}qowheart
+║├─➣  ${prefix}mutgrass
+║├─➣  ${prefix}undergocean
+║├─➣  ${prefix}woodenboards
+║├─➣  ${prefix}wolfmetal
+║├─➣  ${prefix}metalictglow
+║├─➣  ${prefix}8bit
+║├─➣  ${prefix}ttp
+║├─➣  ${prefix}quotemaker
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ MEDIA ◈⊰────»
+║│
+║├─➣  ${prefix}trendtwit
+║├─➣  ${prefix}randomkpop
+║├─➣  ${prefix}ytsearch
+║│
+║╰─────────────── 
+╠═════════════════
+║╭──⊱◈ EDUCAÇÃO ◈⊰────»
+║│
+║├─➣  ${prefix}wiki
+║├─➣  ${prefix}wikien
+║├─➣  ${prefix}nulis
+║├─➣  ${prefix}quotes
+║├─➣  ${prefix}quotes2
+║├─➣  ${prefix}artinama
+║│
+║╰─────────────── 
+╠═════════════════
+║╭──⊱◈ PERGUNTA ◈⊰───»
+║│
+║├─➣  ${prefix}apakah
+║├─➣  ${prefix}kapankah
+║├─➣  ${prefix}rate
+║├─➣  ${prefix}bisakah
+║│
+║╰─────────────── 
+╠═════════════════
+║╭──⊱◈ DOWNLOAD ◈⊰───»
+║│
+║├─➣  ${prefix}pinterest
+║├─➣  ${prefix}ytmp3
+║├─➣  ${prefix}ytmp4
+║├─➣  ${prefix}tiktok
+║├─➣  ${prefix}joox
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ MEME ◈⊰─────»
+║│
+║├─➣  ${prefix}meme
+║├─➣  ${prefix}memeindo
+║│
+║╰─────────────── 
+╠═════════════════
+║╭───⊱◈ GRUPO ◈⊰─────»
+║│
+║├─➣  ${prefix}opengc
+║├─➣  ${prefix}closegc
+║├─➣  ${prefix}promote
+║├─➣  ${prefix}demote
+║├─➣  ${prefix}tagall
+║├─➣  ${prefix}tagall2
+║├─➣  ${prefix}tagall3
+║├─➣  ${prefix}tagall4
+║├─➣  ${prefix}tagall5
+║├─➣  ${prefix}add
+║├─➣  ${prefix}kick
+║├─➣  ${prefix}listadmins
+║├─➣  ${prefix}linkgroup
+║├─➣  ${prefix}leave
+║├─➣  ${prefix}welcome
+║├─➣  ${prefix}nsfw
+║├─➣  ${prefix}leveling
+║├─➣  ${prefix}level
+║├─➣  ${prefix}delete
+║├─➣  ${prefix}simih
+║├─➣  ${prefix}ownergroup
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ SOM ◈⊰────»
+║│
+║├─➣  ${prefix}play
+║├─➣  ${prefix}tts
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ MÚSICA ◈⊰────»
+║│
+║├─➣  ${prefix}lirik
+║├─➣  ${prefix}chord
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ ISLAMISMO ◈⊰─────»
+║│
+║├─➣  ${prefix}quran
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ STALK ◈⊰─────»
+║│
+║├─➣  ${prefix}tiktokstalk
+║├─➣  ${prefix}igstalk
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ WIBU ◈⊰─────»
+║│
+║├─➣  ${prefix}neonime
+║├─➣  ${prefix}pokemon
+║├─➣  ${prefix}loli
+║├─➣  ${prefix}waifu
+║├─➣  ${prefix}randomanime
+║├─➣  ${prefix}husbu
+║├─➣  ${prefix}husbu2
+║├─➣  ${prefix}wait
+║├─➣  ${prefix}nekonime
+║│
+║╰────────────── 
+╠═════════════════
+║╭────⊱◈ 18+ ◈⊰─────»
+║│
+║├─➣  ${prefix}randomhentai
+║├─➣  ${prefix}nsfwtrap
+║├─➣  ${prefix}nsfwneko
+║│
+║╰────────────── 
+╠═════════════════
+║╭────⊱◈ DIVERSÃO ◈⊰─────»
+║│
+║├─➣  ${prefix}alay
+║├─➣  ${prefix}gantengcek
+║├─➣  ${prefix}watak
+║├─➣  ${prefix}hobby
+║├─➣  ${prefix}game
+║├─➣  ${prefix}bucin
+║├─➣  ${prefix}trust
+║├─➣  ${prefix}dare
+║├─➣  ${prefix}simi
+║│
+║╰──────────────── 
+╠═══════════════════
+║╭───⊱◈ IMFORMAÇÃO ◈⊰───»
+║│
+║├─➣  ${prefix}bahasa
+║├─➣  ${prefix}kodenegara
+║├─➣  ${prefix}kbbi
+║├─➣  ${prefix}fakta
+║├─➣  ${prefix}infocuaca
+║├─➣  ${prefix}infogempa
+║├─➣  ${prefix}jadwaltvnow
+║├─➣  ${prefix}covid
+║│
+║╰─────────────── 
+╠═════════════════
+║╭────⊱◈ OWNER ◈⊰────»
+║│
+║├─➣  ${prefix}prefix
+║├─➣  ${prefix}block
+║├─➣  ${prefix}att
+║├─➣  ${prefix}attgp
+║├─➣  ${prefix}clonar
+║├─➣  ${prefix}clearall
+║│
+║╰────────────── 
+╠═════════════════
+║╭───⊱◈ OUTROS ◈⊰─────»
+║│
+║├─➣  ${prefix}send
+║├─➣  ${prefix}wame
+║├─➣  ${prefix}virtex
+║├─➣  ${prefix}exe
+║├─➣  ${prefix}qrcode
+║├─➣  ${prefix}afk
+║├─➣  ${prefix}fml
+║├─➣  ${prefix}fml2
+║│
+║╰───────────────── 
+╠═════════════════ `
+}
+
+exports.help=help
